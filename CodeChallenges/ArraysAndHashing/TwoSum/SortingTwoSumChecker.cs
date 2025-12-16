f7234d2ad123a4aace25baaf9b5ab26292308268
Ryan Robar <ryan.robar@garvin-allen.com>
2025-12-16 13:33:07 -0400

    Started on TwoSum testing

---
+++ CodeChallenges/ArraysAndHashing/TwoSum/SortingTwoSumChecker.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeChallenges.ArraysAndHashing.TwoSum
+{
+    internal class SortingTwoSumChecker : ITwoSumChecker
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var sortedNums = nums
+                .Select((value, index) => new { Value = value, Index = index })
+                .OrderBy(x => x.Value)
+                .ToList();
+
+            int left = 0, right = nums.Length - 1;
+            while (left < right)
+            {
+                int sum = sortedNums[left].Value + sortedNums[right].Value;
+                if (sum == target)
+                {
+                    return [sortedNums[left].Index, sortedNums[right].Index];
+                }
+                else if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return [];
+        }
+    }
+}
---
+++ CodeChallenges/ArraysAndHashing/TwoSum/TwoPassHashMapTwoSumChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenges.ArraysAndHashing.TwoSum
+{
+    internal class TwoPassHashMapTwoSumChecker : ITwoSumChecker
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var indices = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                indices[nums[i]] = i;
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (indices.TryGetValue(complement, out int value) && value != i)
+                {
+                    return [i, value];
+                }
+            }
+
+            return [];
+        }
+    }
+}
